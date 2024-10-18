7b71398c6eb875c112d3fadd8c7806206012316d
cbeaurain <clementbeaurainpro@gmail.com>
2024-10-18 15:40:54 +0200

    setup router + page library

--- hypertube-front/src/App.tsx
+++ hypertube-front/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="text-red-500 text-3xl">Zeubi</div>;
+  return (
+    <div className="w-100 h-[100vh] bg-blue-950 text-white flex justify-center items-center text-3xl">
+      Zeubi
+    </div>
+  );
 }
 
 export default App;
