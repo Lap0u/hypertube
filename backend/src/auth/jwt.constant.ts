35962c3a93bd3abe3e2da10853c0cbff321b9c2d
Augustin LORAIN <alorain@paul-f4Br6s5.clusters.42paris.fr>
2024-10-28 11:41:06 +0100

    cookies in response header

---
+++ backend/src/auth/jwt.constant.ts
@@ -0,0 +1,16 @@
+export const jwtConstants = () => ({
+  accessTokenSecret: process.env.JWT_SECRET,
+  refreshTokenSecret: process.env.JWT_REFRESH_SECRET,
+  accessTokenCookieName: process.env.ACCESS_TOKEN_NAME,
+  refreshTokenCookieName: process.env.REFRESH_TOKEN_NAME,
+  accessTokenCookieConfig: {
+    httpOnly: true,
+    sameSite: true,
+    maxAge: 900000,
+  },
+  refreshTokenCookieConfig: {
+    httpOnly: true,
+    sameSite: true,
+    maxAge: 604800000,
+  },
+});
