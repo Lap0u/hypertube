efa0592fdf3a3720f1a65a71a09d083c1abd8edc
Augustin LORAIN <alorain@paul-f4Br6s5.clusters.42paris.fr>
2024-10-29 17:16:26 +0100

    improve the /movies route

--- backend/src/movies/movies.service.ts
+++ backend/src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { TmdbService } from 'src/tmdb/tmdb.service';
 import { YtsService } from 'src/yts/yts.service';
 
 @Injectable()
@@ -7,10 +8,17 @@ export class MoviesService {
   constructor(
     private prisma: PrismaService,
     private ytsService: YtsService,
+    private tmdbService: TmdbService,
   ) {}
 
   async getMovies(page: number = 1, limit: number = 20) {
-    return await this.ytsService.getMovies(page, limit);
+    const movies = await this.ytsService.getMovies(page, limit);
+    const result = [];
+    for (const movie of movies) {
+      const movieDetails = await this.tmdbService.findMovie(movie.imdbId);
+      result.push({ ...movie, ...movieDetails });
+    }
+    return result;
   }
 
   async getDetails(tmdb_id: number) {}
--- backend/src/tmdb/tmdb.service.ts
+++ backend/src/tmdb/tmdb.service.ts
@@ -9,20 +9,51 @@ export class TmdbService {
   });
 
   async getMovieDetails(tmdb_id: number) {
-    const response = await this.tmdbClient.get(`/movie/${tmdb_id}`);
-    return response.data;
+    const response = await this.tmdbClient.get(
+      `/movie/${tmdb_id}?append_to_response=credits`,
+    );
+    const data = response.data;
+    const details = {
+      tmdbId: data.tmdb_id,
+      imdbId: data.imdb_id,
+      title: data.title,
+      posterUrl: `${process.env.TMDB_IMAGE_URL}${data.poster_path}`,
+      releasedDate: data.release_date,
+      summary: data.overview,
+      cast: data.credits.cast.map((actor) => {
+        return {
+          name: actor.name,
+          character: actor.character,
+          pictureUrl: `${process.env.TMDB_IMAGE_URL}${actor.profile_path}`,
+        };
+      }),
+      crew: data.credits.crew.map((person) => {
+        return {
+          name: person.name,
+          job: person.job,
+          pictureUrl:
+            person.profile_path == null
+              ? null
+              : `${process.env.TMDB_IMAGE_URL}${person.profile_path}`,
+        };
+      }),
+    };
+    return details;
   }
 
   async findMovie(imdb_id: string) {
     const response = await this.tmdbClient.get(
       `/find/${imdb_id}?external_source=imdb_id`,
     );
+    if (response.data.movie_results.length != 1) {
+      return null;
+    }
     const result = response.data.movie_results[0];
     const movie = {
-      tmdb_id: result.id,
+      tmdbId: result.id,
       title: result.title,
-      poster_url: `${process.env.TMDB_IMAGE_URL}${result.poster_path}`,
-      release_date: result.release_date,
+      posterUrl: `${process.env.TMDB_IMAGE_URL}${result.poster_path}`,
+      releaseDate: result.release_date,
     };
     return movie;
   }
