abe76b3d0b3ff632641de3d7aadb19a466e434ad
Augustin LORAIN <alorain@paul-f4Br6s5.clusters.42paris.fr>
2024-11-05 17:40:42 +0100

    apibay find torrents try/catch added

--- backend/src/apibay/apibay.service.ts
+++ backend/src/apibay/apibay.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { extractVideoDetails, humanFileSize } from './utils';
 
@@ -9,24 +9,32 @@ export class ApibayService {
   });
 
   async findTorrents(imdbId: string) {
-    const response = await this.apibayClient.get('/q.php', {
-      params: {
-        q: imdbId,
-      },
-    });
-    const torrents = response.data.map((torrent) => {
-      const { quality, type } = extractVideoDetails(torrent.name);
-      const humanReadableSize = humanFileSize(Number(torrent.size));
-      return {
-        source: 'APIBAY',
-        hash: torrent.info_hash,
-        quality: quality,
-        seeds: Number(torrent.seeders),
-        peers: Number(torrent.leechers),
-        size: humanReadableSize,
-        type: type,
-      };
-    });
-    return torrents;
+    try {
+      const response = await this.apibayClient.get('/q.php', {
+        params: {
+          q: imdbId,
+        },
+      });
+      const torrents = response.data.map((torrent) => {
+        const { quality, type } = extractVideoDetails(torrent.name);
+        const humanReadableSize = humanFileSize(Number(torrent.size));
+        return {
+          source: 'APIBAY',
+          hash: torrent.info_hash,
+          quality: quality,
+          seeds: Number(torrent.seeders),
+          peers: Number(torrent.leechers),
+          size: humanReadableSize,
+          type: type,
+        };
+      });
+      return torrents;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Failed to fetch movie torrents from apibay',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
