5894452cc108a8db0c736c3c6ce56d87be1ff690
Augustin LORAIN <alorain@paul-f4Br4s7.clusters.42paris.fr>
2024-11-07 15:41:40 +0100

    Fix Upload profile picture optional and not download if not valid

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   FileTypeValidator,
@@ -21,19 +22,33 @@ export class UsersController {
 
   @Patch('')
   @ApiBearerAuth()
-  @UseInterceptors(FileInterceptor('profilePicture'))
+  @UseInterceptors(
+    FileInterceptor('profilePicture', {
+      fileFilter: (req, file, callback) => {
+        // Only allow specified file types
+        if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+          return callback(new BadRequestException('Invalid file type'), false);
+        }
+        callback(null, true);
+      },
+    }),
+  )
   @ApiConsumes('multipart/form-data')
   @UseGuards(JwtAccessAuthGuard)
   async patchUser(
     @Body() dto: UpdateUserDto,
-    @UploadedFile(
-      new ParseFilePipe({
-        validators: [new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' })],
-      }),
-    )
+    @UploadedFile()
     profilePicture: Express.Multer.File,
     @Req() req,
   ) {
+    if (profilePicture) {
+      // Apply the validation manually since we need custom logic
+      const isValidFile = new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' })],
+      }).transform(profilePicture);
+
+      if (!isValidFile) throw new BadRequestException('Invalid file format.');
+    }
     const user = req.user;
     delete dto.profilePicture;
     const profilePictureUrl = profilePicture
