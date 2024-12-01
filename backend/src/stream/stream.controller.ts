0c713720538f76be775d60be3a1aad4f97f6a43e
Thomas Berthet <thoberth@student.42.fr>
2024-12-01 12:14:27 +0100

    add header to read strem

--- backend/src/stream/stream.controller.ts
+++ backend/src/stream/stream.controller.ts
@@ -4,24 +4,33 @@ import { Response } from 'express';
 
 @Controller('stream')
 export class StreamController {
-	constructor(
-		private readonly streamService: StreamService
-	) {}
+  constructor(private readonly streamService: StreamService) {}
 
-	@Get('')
-	@Header('Content-type', 'video/mp4')
-	async streamVideo(
-		@Query('magnetLink') magnetLink: string,
-		@Res() res: Response
-	) {	
-		return this.streamService.streamTorrent(magnetLink, false)
-	}
+  @Get('')
+  async streamVideo(
+    @Query('magnetLink') magnetLink: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const stream = await this.streamService.streamTorrent(magnetLink, false);
 
-	@Get('Download')
-	async downloadVideo(
-		@Query('magnetLink') magnetLink: string,
-		@Res() res: Response
-	) {
-		return this.streamService.streamTorrent(magnetLink, true)
-	}
+      res.set({
+        'Content-Type': 'video/mp4', // Adjust based on your use case
+        'Transfer-Encoding': 'chunked',
+      });
+
+      stream.pipe(res);
+    } catch (error) {
+      console.error('Error streaming file:', error);
+      res.status(500).send('Failed to stream the file.');
+    }
+  }
+
+//   @Get('Download')
+//   async downloadVideo(
+//     @Query('magnetLink') magnetLink: string,
+//     @Res() res: Response,
+//   ) {
+//     return this.streamService.streamTorrent(magnetLink, true);
+//   }
 }
