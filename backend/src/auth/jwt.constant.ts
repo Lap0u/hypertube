d7db03300c8dfd722bade32bfe9ec1eba87e6741
cbeaurain <clementbeaurainpro@gmail.com>
2024-12-12 16:47:57 +0100

    feat(): jwt constant timer

--- backend/src/auth/jwt.constant.ts
+++ backend/src/auth/jwt.constant.ts
@@ -6,7 +6,7 @@ export const jwtConstants = () => ({
   accessTokenCookieConfig: {
     httpOnly: true,
     sameSite: true,
-    maxAge: 0,
+    maxAge: 900000,
     withCrendentials: true
   },
   refreshTokenCookieConfig: {
