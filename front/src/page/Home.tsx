57ed06e0408221f67169d041cafdff5a13651ba0
Clement Beaurain <clementbeaurainpro@gmail.com>
2025-01-05 01:44:10 +0100

    home responesive WIP

--- front/src/page/Home.tsx
+++ front/src/page/Home.tsx
@@ -7,20 +7,20 @@ import Footer from '../components/Footer';
 const Home = () => {
   const nav = useNavigate();
   return (
-    <div className="w-100 bg-mainBlack min-g-screen bg-cover min-h-screen">
+    <div className="w-screen bg-mainBlack min-g-screen bg-cover min-h-screen">
       <div
         className="
-     text-white grid grid-cols-2 px-48   justify-center items-center text-3xl bg-bottom gap-24">
-        <h2 className="text-[86px] leading-[6rem] uppercase font-bold title-font ">
+     text-white md:grid grid-cols-2 px-4 md:px-48   flex  flex-col justify-center items-center text-3xl bg-bottom gap-12 md:gap-24">
+        <h2 className="md:text-[86px] md:leading-[6rem] uppercase font-bold title-font ">
           Regardez des{' '}
-          <span className="line-through decoration-red-600 decoration-[16px]  title-font">
+          <span className="line-through decoration-red-600 decoration-[8px] md:decoration[16px]  title-font">
             milliers
           </span>
           &ensp;
           <span className="text-secYellow title-font">millions </span>
           de films et séries en streaming
         </h2>
-        <div className="flex gap-24 justify-center">
+        <div className="flex gap-12 justify-center">
           <Button
             text="LOGIN"
             onClick={() =>
