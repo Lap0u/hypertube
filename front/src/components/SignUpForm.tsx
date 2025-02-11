f263c7941ad3374981a5dadb3f1f2242d1b792b9
Clément Beaurain <clementbeaurainpro@gmail.com>
2025-02-11 13:54:23 +0100

    password front policy ok

--- front/src/components/SignUpForm.tsx
+++ front/src/components/SignUpForm.tsx
@@ -115,11 +115,21 @@ const SignUpForm = () => {
               type="password"
               className="px-4 py-2 text-mainYellow rounded-sm"
               placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;*"
-              {...register('password', { required: true })}
+              {...register('password', {
+                required: true,
+                pattern:
+                  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8  ,}$/,
+              })}
             />
-            {errors.password && (
+            {errors.password && errors.password.type === 'required' && (
               <span className="text-xl mt-[-12px] text-red-600">
                 This field is required
+              </span>
+            )}
+
+            {errors.password && errors.password.type === 'pattern' && (
+              <span className="text-xl mt-[-12px] text-red-600">
+                Your password does not match the pattern
               </span>
             )}
             <input
